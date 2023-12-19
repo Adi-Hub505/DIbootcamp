a18b012c537c152ac406b9de886b0032369a47d4
Adi <adii444@walla.co.il>
2023-12-19 21:10:10 +0200

    week3 day1

---
+++ javascript/script.js
@@ -0,0 +1,65 @@
+let thing = "hello world"
+const thing2 = 4
+
+// console.log(thing);
+
+let a = 5
+let b = true;
+let c = 5+20
+
+let string = `look its the number ${a}`; 
+// console.log (string.indexOf("k"));
+// מיקום של אות מתחיל 0 רק אינדקס ללא אוף
+
+// console.log(true);
+// console.log(a + thing);
+
+// console.log(a*c+(b+a-1));
+
+let first = "This is a very long string which needs "
+
+let longString = first +
+                 "to wrap across multiple lines because " +
+                 "otherwise, my code is unreadable.";
+
+// console.log(longString.length);
+
+let adressnumber = 5
+let adressstreet = "menachem begin"
+let country = "israel"
+let globaladress 
+// console.log(adressnumber , adressstreet , country)
+
+// let name = value
+
+let str = "text"
+let num = 6
+let bool = true
+let v = null
+// let array = [12, 15, 17, "hey"]
+let object = {
+// key: value
+// name: adi
+}
+// log קיצור
+// console.log(str, num);
+
+let sentence = "HOW Are YOU"
+// console.log(sentence.toLowerCase);
+
+// console.log(10.65634345.toFixed(2));
+
+let users = ["adi", "odaya", "greg"[1, 2, 3]]
+
+// רשימה בתוך רשימה
+users.pop()
+users.push("chris")
+
+users.splice(1, 0, "yaffa")
+// 1 2 ממקם את השמות שאחרי, יפה תהיה 
+let first3 = users.slice (0, 3)
+// console.log (first3);
+// console.log(users[1] [3]);
+
+let array = ['cat','dog','fish','rabbit','cow']
+console.log(array[1]);
