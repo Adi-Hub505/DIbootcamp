acbc900cd181db51caae906efae651e67d563683
Adi <adii444@walla.co.il>
2023-12-21 21:16:29 +0200

    week3 day5

---
+++ javascript2/script.js
@@ -0,0 +1,64 @@
+let arr = ['adi', 'odaya', 'yafa']
+
+let obj = {
+    name: "adi",
+    age: 27,
+    online: arr
+}
+
+
+// console.log(obj.age);
+
+let mydata = {
+    username: "adifarada",
+    age: 27,
+    country: "israel"
+    
+}
+mydata.eyeColor = "black"
+mydata.age = 28
+delete mydata.username
+
+// console.log(mydata);
+
+// let userdata =  {
+    // username: "adifarada",
+    // password: "takitaki"
+// }
+
+let myage = 25
+if (19 >= 18){
+
+// console.log("nadav can drink");
+}
+
+// else if (25 > 21){
+    // console.log("nadav can drink, not in us");
+// }
+else if (25 > 18 && 25 < 21){
+    // console.log("nadav can drink, not in us");
+}
+
+
+
+
+else {
+    console.log("nadav cant drink");
+}
+if ("hello" === "25") {
+    // console.log("maybe?");
+}
+
+let password = "a333333333333333334";
+
+if (password.length <= 18) {
+    console.log("password is ok");
+}
+else if (password.length > 18){
+    console.log("password is too long");
+}
+
+else  {
+    console.log("password is too short");
+}
+
